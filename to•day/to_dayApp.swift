e0c12d0795ce8e9fc267cafea612c2341d81a3fb
Garrison Blair <garrisonblair96@gmail.com>
2020-10-13 13:25:58 +0200

    Initial Commit

---
+++ to•day/to_dayApp.swift
@@ -0,0 +1,20 @@
+//
+//  to_dayApp.swift
+//  to•day
+//
+//  Created by Garrison Blair on 2020-10-13.
+//
+
+import SwiftUI
+
+@main
+struct to_dayApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
